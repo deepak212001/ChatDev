0d7649bfd69893d09b5e777783c1c3081f91ff1b
DEEPAK YADAV <103742220+deepak212001@users.noreply.github.com>
2024-05-16 11:31:33 +0530

    Update Context.jsx

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -39,7 +39,7 @@ const ContextProvider = (props) => {
             response = await runChat(input)
         }
         let responseArray = response.split("**")
-        let newResponse;
+        let newResponse=";
         for (let i = 0; i < responseArray.length; i++) {
             if (i === 0 || i % 2 !== 1) {
                 newResponse += responseArray[i];
